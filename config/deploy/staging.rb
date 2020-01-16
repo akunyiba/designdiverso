aa9c9569a47c5ad474550fb74291853f58c199dc
Philip <akunyiba1@gmail.com>
2020-01-16 04:26:25 +0300

    first commit

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -18,7 +18,7 @@ server '92.53.96.249', user: 'godlike2046', roles: %w{web app db}
    keys: %w(~/.ssh/id_rsa),
    forward_agent: true,
    user: fetch(:user),
-   auth_methods: %w(password)
+   auth_methods: %w(publickey)
  }
 
 fetch(:default_env).merge!(wp_env: :staging)
